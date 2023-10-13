602aed06cd0cd72d60b1d6697944e6428ed6f03b
Marcelrp <marcel.pie@hotmail.com>
2023-10-13 11:27:21 -0300

    todolist terminado

---
+++ src/main/java/br/com/mroberto/todolist/Utils/Utils.java
@@ -0,0 +1,39 @@
+package br.com.mroberto.todolist.Utils;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Utils {
+
+    public static void copyNonNullProperties(Object source, Object target) {
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+
+    public static String[] getNullPropertyNames(Object source) {
+
+        final BeanWrapper beanWrapper = new BeanWrapperImpl(source);
+
+         PropertyDescriptor[] pds = beanWrapper.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+
+        for (PropertyDescriptor pd: pds) {
+
+            Object sourceValue = beanWrapper.getPropertyValue( pd.getName() );
+
+            if(sourceValue == null) {
+
+                emptyNames.add(pd.getName());
+           }
+        }
+
+        String[] result = new String[emptyNames.size()];
+
+        return emptyNames.toArray(result);
+    }
+}
